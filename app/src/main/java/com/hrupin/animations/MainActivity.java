51ab4808b1e36141667a3026d2bcc55bee1b8ada
Igor Khrupin <igor@hrupin.com>
2016-09-28 14:24:48 +0200

    added animation adapter for RecycleView

--- app/src/main/java/com/hrupin/animations/MainActivity.java
+++ app/src/main/java/com/hrupin/animations/MainActivity.java
@@ -1,13 +1,43 @@
 package com.hrupin.animations;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.animation.OvershootInterpolator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String[] data = new String[]{
+            "Apple", "Ball", "Camera", "Day", "Egg", "Foo", "Google", "Hello", "Iron", "Japan", "Coke",
+            "Dog", "Cat", "Yahoo", "Sony", "Canon", "Fujitsu", "USA", "Nexus", "LINE", "Haskell", "C++",
+            "Java", "Go", "Swift", "Objective-c", "Ruby", "PHP", "Bash", "ksh", "C", "Groovy", "Kotlin",
+            "Chip", "Japan", "U.S.A", "San Francisco", "Paris", "Tokyo", "Silicon Valley", "London",
+            "Spain", "China", "Taiwan", "Asia", "New York", "France", "Kyoto", "Android", "Google",
+            "iPhone", "iPad", "iPod", "Wasabeef"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_adapter_sample);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        MainAdapter adapter = new MainAdapter(this, new ArrayList<>(Arrays.asList(data)));
+        adapter.setFirstOnly(true);
+        adapter.setDuration(500);
+        adapter.setInterpolator(new OvershootInterpolator(.5f));
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/hrupin/animations/MainAdapter.java
@@ -0,0 +1,119 @@
+package com.hrupin.animations;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<String> mDataSet;
+    private static final float DEFAULT_SCALE_FROM = .5f;
+    private float mFrom = DEFAULT_SCALE_FROM;
+    private int mDuration = 300;
+    private Interpolator mInterpolator = new LinearInterpolator();
+    private int mLastPosition = -1;
+    private boolean isFirstOnly = true;
+
+    public MainAdapter(Context context, List<String> dataSet) {
+        mContext = context;
+        mDataSet = dataSet;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.layout_list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.text.setText(mDataSet.get(position));
+
+        int adapterPosition = holder.getAdapterPosition();
+        if (!isFirstOnly || adapterPosition > mLastPosition) {
+            for (Animator anim : getAnimators(holder.itemView)) {
+                anim.setDuration(mDuration).start();
+                anim.setInterpolator(mInterpolator);
+            }
+            mLastPosition = adapterPosition;
+        } else {
+            clear(holder.itemView);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    public void remove(int position) {
+        mDataSet.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void add(String text, int position) {
+        mDataSet.add(position, text);
+        notifyItemInserted(position);
+    }
+
+    public void setDuration(int duration) {
+        mDuration = duration;
+    }
+
+    public void setInterpolator(Interpolator interpolator) {
+        mInterpolator = interpolator;
+    }
+
+    public void setStartPosition(int start) {
+        mLastPosition = start;
+    }
+
+    private Animator[] getAnimators(View view) {
+        ObjectAnimator scaleX = ObjectAnimator.ofFloat(view, "scaleX", mFrom, 1f);
+        ObjectAnimator scaleY = ObjectAnimator.ofFloat(view, "scaleY", mFrom, 1f);
+        return new ObjectAnimator[]{scaleX, scaleY};
+    }
+
+    public void setFirstOnly(boolean firstOnly) {
+        isFirstOnly = firstOnly;
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView image;
+        public TextView text;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = (ImageView) itemView.findViewById(R.id.image);
+            text = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+
+    public static void clear(View v) {
+        ViewCompat.setAlpha(v, 1);
+        ViewCompat.setScaleY(v, 1);
+        ViewCompat.setScaleX(v, 1);
+        ViewCompat.setTranslationY(v, 0);
+        ViewCompat.setTranslationX(v, 0);
+        ViewCompat.setRotation(v, 0);
+        ViewCompat.setRotationY(v, 0);
+        ViewCompat.setRotationX(v, 0);
+        ViewCompat.setPivotY(v, v.getMeasuredHeight() / 2);
+        ViewCompat.setPivotX(v, v.getMeasuredWidth() / 2);
+        ViewCompat.animate(v).setInterpolator(null).setStartDelay(0);
+    }
+}
