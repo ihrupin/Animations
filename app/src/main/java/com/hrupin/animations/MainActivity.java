15ead561de134c17f84e93cef2928d90d2dce486
Igor Khrupin <igor@hrupin.com>
2016-09-28 18:14:38 +0200

    AppBarLayout animation

--- app/src/main/java/com/hrupin/animations/MainActivity.java
+++ app/src/main/java/com/hrupin/animations/MainActivity.java
@@ -1,5 +1,8 @@
 package com.hrupin.animations;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabItem;
@@ -7,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.support.design.widget.TabLayout;
 import android.widget.Toast;
@@ -25,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
             "iPhone", "iPad", "iPod", "Wasabeef"
     };
     private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private RecyclerView recyclerView;
+    private MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +43,15 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        appBarLayout = (AppBarLayout)findViewById(R.id.app_bar_layout);
+        appBarLayout.setVisibility(View.VISIBLE);
+        appBarLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                animateDiagonalPan(appBarLayout);
+            }
+        });
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.addTab(tabLayout.newTab().setText("ALL"));
@@ -46,11 +60,54 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         tabLayout.addTab(tabLayout.newTab().setText("IN TRANSIT"));
         tabLayout.setOnTabSelectedListener(this);
 
-        MainAdapter adapter = new MainAdapter(this, new ArrayList<>(Arrays.asList(data)));
+        adapter = new MainAdapter(this, new ArrayList<>(Arrays.asList(data)));
         adapter.setFirstOnly(false);
-        adapter.setDuration(100);
+        adapter.setDuration(400);
         adapter.setInterpolator(new OvershootInterpolator(.5f));
+
+        recyclerView = (RecyclerView) findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+        recyclerView.setVisibility(View.INVISIBLE);
+    }
+
+    private void animateDiagonalPan(View v) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(v, "translationY", -v.getHeight(), 0);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(300);
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                if(appBarLayout != null){
+                    appBarLayout.setVisibility(View.VISIBLE);
+                }
+                if(recyclerView != null){
+                    recyclerView.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if(appBarLayout != null){
+                    appBarLayout.setVisibility(View.VISIBLE);
+                }
+                if(recyclerView != null){
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        animator.start();
     }
 
     @Override
